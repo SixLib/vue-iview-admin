6efdb41a4b00413702e79521181a1f5c86bf964c
Kenny <614044401@qq.com>
2018-08-28 22:10:43 +0800

    拆分组件

---
+++ src/layout/components/side.vue
@@ -0,0 +1,23 @@
+<template>
+    <Menu :active-name="active" theme="dark" width="auto" :class="menuitemClasses">
+        <MenuItem name="dashboard" to='/'>
+            <Icon type="ios-home"></Icon>
+            <span>首页</span>
+        </MenuItem>
+        <MenuItem name="documentation" to='/documentation'>
+            <Icon type="ios-document" />
+            <span>文档</span>
+        </MenuItem>
+    </Menu>
+</template>
+<script>
+export default {
+  props: ["active","isCollapsed"],
+  computed: {
+    menuitemClasses() {
+      return ["menu-item", this.isCollapsed ? "collapsed-menu" : ""];
+    }
+  }
+};
+</script>
+
--- src/layout/layout.vue
+++ src/layout/layout.vue
@@ -1,37 +1,30 @@
 <template>
     <div class="layout">
         <Layout style="height:100%;">
-            <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
-                <Menu :active-name="active" theme="dark" width="auto" :class="menuitemClasses">
-                  <MenuItem name="dashboard" to='/'>
-                      <Icon type="ios-home"></Icon>
-                      <span>首页</span>
-                  </MenuItem>
-                  <MenuItem name="documentation" to='/documentation'>
-                      <Icon type="ios-document" />
-                      <span>文档</span>
-                  </MenuItem>
-                </Menu>
-            </Sider>
-            <Layout>
-                <Header :style="{padding: 0}" class="layout-header-bar">
-                  <i-Header :breadcrumbs="breadcrumbs" :isCollapsed="isCollapsed" v-on:changeProps="listenChangeHeaderProps"/>
-                </Header>
-                   <!-- <tags :tags="tags"/> -->
-               <Content :style="{margin: '2px', background: '#fff'}">
-                    <router-view v-on:changeProps="listenChangeProps"></router-view>
-                </Content>
-            </Layout>
+          <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
+            <i-Sider :isCollapsed="isCollapsed" :active="active"/>
+          </Sider>
+          <Layout>
+              <Header :style="{padding: 0}" class="layout-header-bar">
+                <i-Header :breadcrumbs="breadcrumbs" :isCollapsed="isCollapsed" v-on:changeProps="listenChangeHeaderProps"/>
+              </Header>
+                  <!-- <tags :tags="tags"/> -->
+              <Content :style="{margin: '2px', background: '#fff'}">
+                  <router-view v-on:changeProps="listenChangeProps"></router-view>
+              </Content>
+          </Layout>
         </Layout>
     </div>
 </template>
 <script>
 import "./layout.css";
+import side from "./components/side.vue";
 import header from "./components/header.vue";
 import tags from "./components/tags.vue";
 export default {
   components: {
     "i-Header": header,
+    "i-Sider": side,
     tags
   },
   data() {
@@ -42,11 +35,6 @@ export default {
       tags: [],
       tabsVal: ""
     };
-  },
-  computed: {
-    menuitemClasses() {
-      return ["menu-item", this.isCollapsed ? "collapsed-menu" : ""];
-    }
   },
   methods: {
     listenChangeHeaderProps() {
